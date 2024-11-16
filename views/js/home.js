3b5bc543b6c95fa7ed423d1cd9d5e6106164685b
Avazbek <avazjonjumoqulov@gmail.com>
2024-11-16 18:01:08 +0500

    comfortable section qo'shildi

--- views/js/home.js
+++ views/js/home.js
@@ -9,3 +9,42 @@ menu_btn.addEventListener('click', function () {
         menu_btn.innerHTML = '<i class="ri-menu-line"></i>';
     }
 })
+
+/* CAROUSEL */
+
+$(document).ready(function () {
+    $('.carousel').slick({
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        prevArrow: '<button class="slick-prev"><i class="ri-arrow-left-s-line"></i></button>',
+        nextArrow: '<button class="slick-next"><i class="ri-arrow-right-s-line"></i></button>',
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 2,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
